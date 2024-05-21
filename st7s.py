f2b21914d621a922004ba06b3d2dbff703c71317
ZEVS92 <i@seemask.ru>
2024-05-21 14:45:40 +0300

    Create st7s.py

---
+++ st7s.py
@@ -0,0 +1,8 @@
+import time
+from colorama import Fore, init
+init()
+
+print(Fore.RED+"https://github.com/ZEVS92/desc_linux_station/")
+print(Fore.YELLOW+"Testing Functions...")
+time.sleep(5)
+print(Fore.RED+"UPDATE PROGRAM 01.06.24! PLS WAIT:)")
